4ebebf5cfbedda2768fc48cf99dec43fceb5d812
chromato99 <chromato99@gmail.com>
2023-03-13 21:43:25 +0900

    Change contents not go out of the screen

--- src/components/TeamInfo.js
+++ src/components/TeamInfo.js
@@ -3,54 +3,40 @@ import React from 'react'
 export default function TeamInfo() {
   return (
     <div id='d0'>
-      <div class="text_1">
+      <div className="text_1">
         <h1>Team members</h1>
       </div>
-
       <div id='d1'>
-
-        <div class='mem_group'>
-          <img name="seungil" class = "profile_img" src= {require("../assets/Seungil.png")} width="300" height="300" alt="profile"/>
-          <p class="text_name"><b>Seungil Han</b></p>
+        <div className='mem_group'>
+          <img name="seungil" className="profile_img" src= {require("../assets/Seungil.png")} alt="profile"/>
+          <p className="text_name"><b>Seungil Han</b></p>
           <p>thesence01@gachon.ac.kr</p>
         </div>
 
-        <div class='mem_group'>
-          <img name="jimin" class = "profile_img" src= {require("../assets/Jimin.png")} width="300" height="300" alt="profile"/>
-          <p class="text_name"><b>Jimin Lee</b></p>
+        <div className='mem_group'>
+          <img name="jimin" className = "profile_img" src= {require("../assets/Jimin.png")}  alt="profile"/>
+          <p className="text_name"><b>Jimin Lee</b></p>
           <p>reakin@gachon.ac.kr</p>
         </div>
         
-        <div class='mem_group'>
-        <img name="doyeon" class = "profile_img" src= {require("../assets/Doyeon.png")} width="300" height="300" alt="profile"/>
-          <p class="text_name"><b>Doyeon Hyun</b></p>
+        <div className='mem_group'>
+        <img name="doyeon" className = "profile_img" src= {require("../assets/Doyeon.png")} alt="profile"/>
+          <p className="text_name"><b>Doyeon Hyun</b></p>
           <p>118ssun@gachon.ac.kr</p>
         </div>
 
-        <div class='mem_group'>
-          <img name="bumgi" class = "profile_img" src= {require("../assets/Beomgi.png")} width="300" height="300" alt="profile"/>
-          <p class="text_name"><b>Beomgi Kim</b></p>
+        <div className='mem_group'>
+          <img name="bumgi" className = "profile_img" src= {require("../assets/Beomgi.png")} alt="profile"/>
+          <p className="text_name"><b>Beomgi Kim</b></p>
           <p>chromato99@gachon.ac.kr</p>
         </div>
 
-        <div class='mem_group'> 
-          <img name="yejin" class = "profile_img" src= {require("../assets/Yejin.png")} width="300" height="300" alt="profile"/>
-          <p class="text_name"><b>Yejin Choi</b></p>
+        <div className='mem_group'> 
+          <img name="yejin" className = "profile_img" src= {require("../assets/Yejin.png")} alt="profile"/>
+          <p className="text_name"><b>Yejin Choi</b></p>
           <p>chldppwls12@gachon.ac.kr</p>
         </div>
-
-       
-
-        
-
       </div>
-     
-      
-      
-     
-
     </div>
-
-    
   )
 }
