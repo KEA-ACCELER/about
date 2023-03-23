ff7a1c938107f6f6366183827994666f89a2f0c7
Beomgi Kim | 김범기 <chromato99@gmail.com>
2023-03-23 22:20:46 +0900

    add: github actions for ci/cd (#13)

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Home.js', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Github/i);
   expect(linkElement).toBeInTheDocument();
 });
