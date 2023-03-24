d864f9c41de20105b1aa855ae8c8ed0eace2c277
chromato99 <chromato99@gmail.com>
2023-03-24 16:02:02 +0900

    update: use useNavigate() for faster page switching

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,16 +2,17 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { FaGithub } from 'react-icons/fa';
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function Layout() {
+function Layout() {    
+  const navigate = useNavigate();
   return (
     <div>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="/">
+          <Navbar.Brand onClick={() => navigate("/")}>
             <div id="cropped_logo_acceler">
               <img
                 className="logo_acceler"
@@ -23,10 +24,10 @@ function Layout() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="vision">Vision</Nav.Link>
-              <Nav.Link href="teaminfo">Team Info</Nav.Link>
-              <Nav.Link href="contact">Contact</Nav.Link>
+              <Nav.Link onClick={() => navigate("/")} >Home</Nav.Link>
+              <Nav.Link onClick={() => navigate("vision")} >Vision</Nav.Link>
+              <Nav.Link onClick={() => navigate("teaminfo")} >Team Info</Nav.Link>
+              <Nav.Link onClick={() => navigate("contact")} >Contact</Nav.Link>
               <NavDropdown title="Projects" id="basic-nav-dropdown">
                 <NavDropdown.Item href="https://aform.acceler.kr">A-Form</NavDropdown.Item>
               </NavDropdown>
